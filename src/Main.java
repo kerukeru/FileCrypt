a34569512bc9ab3985e24dddae4fe03996e00492
keru <keru@protonmail.com>
2019-07-05 21:39:07 +0200

    initiAL COMMIT

--- src/Main.java
+++ src/Main.java
@@ -17,7 +17,7 @@ public class Main {
         try {
             byte[] fileContent = Files.readAllBytes(plik.toPath());
 
-            for(byte myValue: fileContent){ System.out.println("plik: "+myValue); }
+            //for(byte myValue: fileContent){ System.out.println("plik: "+myValue); }
 
             try (FileOutputStream stream = new FileOutputStream("/home/keru/Pulpit/output.pdf")) {
                 stream.write(fileContent);
@@ -29,7 +29,18 @@ public class Main {
 //
         //FileUtils.writeByteArrayToFile(new File("pathname"), myByteArray);
 
+        String key = "Mary has one cat";
+        File inputFile = new File("/home/keru/Pulpit/crypt_test/doc.txt");
+        File encryptedFile = new File("/home/keru/Pulpit/crypt_test/document.encrypted");
+        File decryptedFile = new File("/home/keru/Pulpit/crypt_test/document.decrypted");
 
+        try {
+            CryptoUtils.encrypt(key, inputFile, encryptedFile);
+            CryptoUtils.decrypt(key, encryptedFile, decryptedFile);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            ex.printStackTrace();
+        }
 
 
     }
